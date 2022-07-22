4bdf88d86df09a121cdc61331a21fe73ba324c83
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2022-07-22 21:41:10 +0530

    day-4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import "./App.css";
+import './App.css';
+
 
 function App() {
   return (
     <div>
+  
 
     </div>
   );
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -7,12 +7,15 @@
 import thunk from "redux-thunk";
 import { authReducer } from "./AuthReducer/auth.reducer";
 import { dashboardreducer } from "./Dashboard/reducer";
+import {BookCarReducer} from './BookingCar/reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer,
   dashboard : dashboardreducer,
+  mybookcars: BookCarReducer,
+
 });
 
  export const store = legacy_createStore(
