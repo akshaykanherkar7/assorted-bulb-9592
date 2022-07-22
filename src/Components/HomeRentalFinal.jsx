fa5e4afd388b2140a11120e655ab9272c74618f4
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-07-22 21:36:40 +0530

    final-homerental

---
+++ src/Components/HomeRentalFinal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CarouselBanner from './CarouselBanner'
+import SearchBox from './SearchBox'
+
+
+const HomeRentalFinal = () => {
+  return (
+    <div>
+        <SearchBox/>
+        <CarouselBanner/>
+    </div>
+  )
+}
+
+export default HomeRentalFinal
